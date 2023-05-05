d5ef37ba96bcfe3cdf88b2c750953346cb4696b3
Rittza <111962664+Rittza@users.noreply.github.com>
2023-05-05 11:15:16 +0200

    Add project files.

---
+++ Get set/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Get_set;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_set
+{
+    class Minklass
+    {
+
+        private string mc;
+
+
+        public string Mc
+        {
+            get { return mc; }
+            set { mc = value; }
+        }
+
+    }
+
+    class Bil
+    {
+        private string bilmärke;
+        private int årsmodell;
+        private string modell;
+
+        public Bil(string b, int å, string m)
+        {
+            bilmärke = b;
+            årsmodell = å;
+            modell = m;
+        }
+
+        public string b
+        {
+            get { return bilmärke;}
+            set { bilmärke = value; }
+        }
+
+        public int å
+        {
+            get { return årsmodell; }
+            set { årsmodell = value; }
+        }
+
+        public string m
+        {
+            get { return modell; }
+            set { modell = value; }
+        }
+
+    }
+}
---
+++ Get set/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Get_set;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Minklass make = new Minklass();
+            Minklass cc = new Minklass();
+            Minklass hp = new Minklass();
+
+
+            Console.WriteLine("Skriv in ett MC-Märke märke:");
+            make.Mc = Console.ReadLine();
+
+            Console.WriteLine("Skriv in motorns cc");
+            cc.Mc = Console.ReadLine();
+
+            Console.WriteLine("Skriv in hästkraften");
+            hp.Mc = Console.ReadLine();
+
+            Console.WriteLine("Motorcykeln är en" + " " + make.Mc + " " + "på " + cc.Mc + " " + "cc" + " " + "med" + " " + hp.Mc + "hp");
+
+            Bil p = new Bil("BMW", 1980, "M3");
+            Bil p1 = new Bil("Mercedes", 2005, "C63");
+            Bil p2 = new Bil("Audi", 2018, "RS6");
+
+            Console.WriteLine(p.b + " " + p.å + " " + p.m);
+            Console.WriteLine(p1.b + " " + p1.å + " " + p1.m);
+            Console.WriteLine(p2.b + " " + p2.å + " " + p2.m);
+
+        }
+    }
+}
